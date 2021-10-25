cfe8c4515487d02dfcf16b6f42bd434329152444
Fabio <fabiomdg1@gmail.com>
2021-10-25 17:46:49 -0300

    Padronização de comentários, menu e banner nas páginas cadastro, serviço e cadastro

---
+++ js/script_index.js
@@ -0,0 +1,33 @@
+// Função para que o nome da empresa que fica sobre o banner seja escrito ao carregar a página
+let j = 0;
+let txt = "G8 Tecnologia"
+function escrever() {
+    if (j < txt.length) {
+        document.getElementById("nome").innerHTML += txt.charAt(j);
+        j++;
+        setTimeout(escrever, 100);
+    }
+}
+
+// Função para imagens rotativas na área de "Nossos Trabalhos"
+$(document).ready(function () {
+    let vtBanner = ["imagens/banner1.png", "imagens/banner2.png", "imagens/banner3.png", "imagens/banner4.png", "imagens/banner5.png"];
+    let max = vtBanner.length - 1;
+    let i = 0;
+
+    $("#divNT2").css("backgroundImage", "url(" + vtBanner[0] + ")");
+
+    setInterval(() => troca(1), 3000)
+
+    function troca(opr) {
+        $("#divNT2").css("backgroundImage", "url(" + vtBanner[i] + ")").fadeOut(1000, function () {
+            i += opr;
+            if (i > max) {
+                i = 0;
+            } else if (i < 0) {
+                i = max;
+            }
+            $("#divNT2").css("backgroundImage", "url(" + vtBanner[i] + ")").fadeIn(1000);
+        });
+    }
+})
